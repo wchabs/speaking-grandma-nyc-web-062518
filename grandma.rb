e875fb4a2cdc4a60d2ff65e00350a2fd53e06316
William Chabla <w_chabla@hotmail.com>
2018-06-21 04:58:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA"
-    puts "I LOVE YOU TOO PUMPKIN"
+    "I LOVE YOU TOO PUMPKIN"
     elsif phrase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else
-    puts "HUH! SPEAK UP, SONNY!"
+    "HUH! SPEAK UP, SONNY!"
   end
 end
